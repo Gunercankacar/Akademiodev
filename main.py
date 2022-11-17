b31bccd93a2c1321a5b79a8655736a92c19b9c01
Gunercankacar <118397338+Gunercankacar@users.noreply.github.com>
2022-11-17 11:04:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+baslik = "Hosgeldiniz"
+print(baslik)
+vade = 10
+faizOrani = 1.47 
+print(faizOrani)
+print(vade)
+print(type(vade)) 
+print(type(faizOrani))
+print(type(baslik))
+
+mesaj = "Merhaba"
+musteriAdi ="Can"
+musteriSoyadi ="Kaçar"
+
+print(mesaj+ " "+musteriAdi+ " "+musteriSoyadi)
+
+sonucMesaji = mesaj +" " + musteriAdi +" "+ musteriSoyadi
+
+print(sonucMesaji)
+
+print(f'{mesaj} {musteriAdi} {musteriSoyadi}')
+sayi1 = 20
+sayi2 = 15
+sonucToplam = sayi1 + sayi2 
+sonucCikarma = sayi1 - sayi2 
+print(sonucToplam)
+print(sonucCikarma)
